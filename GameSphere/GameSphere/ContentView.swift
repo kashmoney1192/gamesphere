5f82e9fe1bee459353fb257a94bf1b0920fae8cd
AiGames <kashmoney571192@gmail.com>
2025-11-25 12:16:15 -0500

    Load GameSphere games directly in macOS app - no link redirect
    - Use NSViewRepresentable for macOS WKWebView support
    - Display full arcade game collection in native macOS app
    - Load bundled index.html directly without fallback link
    - Works seamlessly on both iOS and macOS

--- GameSphere/GameSphere/ContentView.swift
+++ GameSphere/GameSphere/ContentView.swift
@@ -6,27 +6,40 @@
 //
 
 import SwiftUI
-
-#if os(iOS)
 import WebKit
 
+#if os(iOS)
 struct WebViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
-
-        if let htmlPath = Bundle.main.path(forResource: "index", ofType: "html") {
-            let url = URL(fileURLWithPath: htmlPath)
-            webView.load(URLRequest(url: url))
-        } else {
-            if let url = URL(string: "https://kashmoney1192.github.io/gamesphere/") {
-                webView.load(URLRequest(url: url))
-            }
-        }
-
+        loadHTML(into: webView)
         return webView
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
+    }
+}
+#else
+struct WebViewContainer: NSViewRepresentable {
+    func makeNSView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        loadHTML(into: webView)
+        return webView
+    }
+
+    func updateNSView(_ webView: WKWebView, context: Context) {
+    }
+}
+#endif
+
+func loadHTML(into webView: WKWebView) {
+    if let htmlPath = Bundle.main.path(forResource: "index", ofType: "html") {
+        let url = URL(fileURLWithPath: htmlPath)
+        webView.load(URLRequest(url: url))
+    } else {
+        if let url = URL(string: "https://kashmoney1192.github.io/gamesphere/") {
+            webView.load(URLRequest(url: url))
+        }
     }
 }
 
@@ -36,21 +49,6 @@ struct ContentView: View {
             .ignoresSafeArea()
     }
 }
-
-#else
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("GameSphere Arcade")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Text("Open https://kashmoney1192.github.io/gamesphere/ in your web browser")
-                .font(.body)
-                .padding()
-        }
-    }
-}
-#endif
 
 #Preview {
     ContentView()
